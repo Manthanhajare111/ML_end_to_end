bf9abda16c545ab60f96d81138b851b0cddc1e8b
manteshwarhajare <manteshwarhajare@gmail.com>
2023-04-12 20:00:23 +0530

    Hyperparameter Tunning with diff models

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -46,27 +46,20 @@ class ModelTrainer:
                 "Decision Tree": DecisionTreeRegressor(),
                 "Gradient Boosting": GradientBoostingRegressor(),
                 "Linear Regression": LinearRegression(),
-                "K-Neighbors Classifier": KNeighborsRegressor(),
-                "XGBClassifier": XGBRegressor(),
+                # "K-Neighbors Classifier": KNeighborsRegressor(),
+                "XGBRegressor": XGBRegressor(),
                 # "Cat-Boosting Classifier": CatboostRegressor(verbose=True),
-                "AdaBoost Classifier": AdaBoostRegressor()
+                "AdaBoost Regressor": AdaBoostRegressor()
                 }
             params = {
                 "Decision Tree":
                 {"criterion":['squared_error','friedman_mse','absolute_error','poisson'],
-                 # 'splitter':['best','random'],
-                 # 'max_features':['sqrt','log2']
-                 },
+                },
                 "Random Forest":{
-                # "criterion":['squared_error','friedman_mse','absolute_error','poisson'],
-                 # 'max_features':['sqrt','log2','None'],
                  'n_estimators':[8,16,32,64,128,256]},
                 "Gradient Boosting":{
-                # "loss":['squared_error','huber','absolute_error','quantile'],
                     'learning_rate':[.1,.01,.05,.001],
                     'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
-                    # 'criterion':['squared_error','friedman_mse'],
-                    # 'max_features':['squared_error','friedman_mse'],
                     'n_estimators':[8,16,32,64,128,256]},
                 "Linear Regression":{},
                 "XGBRegressor":{
--- src/utils.py
+++ src/utils.py
@@ -10,6 +10,8 @@ from src.exception import CustomException
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score
 
+from sklearn.model_selection import GridSearchCV
+
 def save_object(file_path,obj):
     try:
         
@@ -31,15 +33,15 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            # para=param[list(models.keys())[i]]
+            para=param[list(models.keys())[i]]
 
-            # gs = GridSearchCV(model,para,cv=3)
-            # gs.fit(X_train,y_train)
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
 
-            # model.set_params(**gs.best_params_)
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
 
-            #model.fit(X_train, y_train)  # Train model
+            # model.fit(X_train, y_train)  # Train model
 
             y_train_pred = model.predict(X_train)
 
